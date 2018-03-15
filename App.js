ec28c261e0ede796e142df80ca4f5465db857b56
Amit Kumar Swami <aks.swami@live.com>
2018-03-15 15:07:36 +0800

    Create custom component and show loader and error depending on API call

--- App.js
+++ App.js
@@ -22,6 +22,8 @@ class App extends Component {
 
   searchSubmit = async () => {
     try {
+      this.setState({loading: true})
+      
       const response = await fetch('https://api.github.com/users/'+ this.state.username +'/repos')
       const repos = await response.json()
 
@@ -31,31 +33,10 @@ class App extends Component {
     }
   }
 
-  renderItem = ({item, index}) => {
-    return (
-
-      <View style={styles.repo}>
-      <View style={styles.repoNumber}>
-          <Text>
-            {index + 1}
-          </Text>
-        </View>
-        <View style={styles.repoContent}>
-          <Text>
-            {item.name}
-          </Text>
-          <Text style={styles.repoBody}>
-            {item.name}
-          </Text>
-        </View>
-      </View>
-    )
-  }
-
   render() {
     const {repos, loading, error} = this.state
     return (
-          <View style={styles.center}>
+        <View style={styles.center}>
           <TextInput
             style={{height: 40, padding: 5, margin: 5}}
             onChangeText={(username) => this.setState({username})}
@@ -65,29 +46,11 @@ class App extends Component {
             clearButtonMode="while-editing"
           />
           <View style={{height: 1, backgroundColor: 'gray'}}/>
-          <FlatList
-            style={styles.listContainer}
-            data={repos}
-            renderItem={this.renderItem}
-            keyExtractor={extractKey}
-            />
-</View>
-      // if (loading) {
-      //     <View style={styles.center}>
-      //       <ActivityIndicator animating={true} />
-      //     </View>
-      // } else if (error) {
-      //     <View style={styles.center}>
-      //       <Text>
-      //         Failed to load repos!
-      //       </Text>
-      //     </View>
-      // } else {
-
-
-
-      // </View>
-
+          <Welcome loading={loading}
+            error = {error}
+            repos = {repos}
+            style={styles.listContainer}/>
+        </View>
     )
   }
 }
@@ -133,5 +96,52 @@ const styles = StyleSheet.create({
     backgroundColor: 'skyblue',
   },
 })
+
+function Welcome(props) {
+  renderItem = ({item, index}) => {
+       return (
+         <View style={styles.repo}>
+         <View style={styles.repoNumber}>
+             <Text>
+               {index + 1}
+             </Text>
+           </View>
+           <View style={styles.repoContent}>
+             <Text>
+               {item.name}
+             </Text>
+             <Text style={styles.repoBody}>
+               {item.description}
+             </Text>
+           </View>
+         </View>
+       )
+     }
+
+  if (props.loading) {
+   return (
+    <View style={styles.center}>
+      <ActivityIndicator animating={true} />
+    </View>
+    )
+  } else if (props.error) {
+    return (
+      <View style={styles.center}>
+        <Text>
+          Failed to load repos!
+        </Text>
+      </View>
+    )
+  } else {
+    return (
+      <FlatList
+        style={styles.listContainer}
+        data={props.repos}
+        renderItem={this.renderItem}
+        keyExtractor={extractKey}
+        />
+    )
+  }
+}
 
 AppRegistry.registerComponent('App', () => App)
